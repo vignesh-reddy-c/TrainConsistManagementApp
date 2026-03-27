0dd7985e45f088a85d63a33cdab8a100746a3073
vignesh-reddy-c <ga6626192@gmail.com>
2026-03-27 16:38:47 +0530

    UC3: Track Unique Bogie IDs (Set – HashSet)

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,9 +1,18 @@
 import java.util.*;
-class TrainConsistManagementApp {
+
+public class TrainConsistManagementApp {
     public static void main(String[] args) {
         System.out.println("=== Train Consist Management App ===");
-        List<String> bogies = new ArrayList<>();
-        System.out.println("Train consist initialized.");
-        System.out.println("Initial bogie count: " + bogies.size());
+        Set<String> bogieIds = new HashSet<>();
+        bogieIds.add("BG101");
+        bogieIds.add("BG102");
+        bogieIds.add("BG103");
+        System.out.println("Adding duplicate ID: BG101");
+        boolean isAdded = bogieIds.add("BG101");
+        if (!isAdded) {
+            System.out.println("Duplicate Bogie ID detected! Registration failed for: BG101");
+        }
+        System.out.println("Unique Bogie IDs in System: " + bogieIds);
+        System.out.println("Total Unique Bogies: " + bogieIds.size());
     }
 }
